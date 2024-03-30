1a40a0dc650bac6cfce7c067568f6bbac4a22a23
Rafi Fikrian N <117027265+rafifikri24@users.noreply.github.com>
2024-03-30 11:57:40 +0700

    Update page.js

--- control/page.js
+++ control/page.js
@@ -1122,6 +1122,21 @@ const SetujuPinjam = (req,res) =>{
 
   }
 
+const Getme = ( req, res ) => {
+  role = req.user.role
+   if(role == 'admin'){
+    const location = '/admin/home'
+    res.send(location)
+  }else if(role =='user'){
+    const query = `SELECT no_induk FROM siswa WHERE username = '${req.user.username}'`
+    db.query(query,(err,result) =>{
+        const location = `/${result[0].no_induk}/buku/daftarbuku`
+        res.send(location)
+    });
+  }
+
+}
+
 
 module.exports = { Tampilbuku, Tambahbuku, Editbuku, Hapusbuku,TampilbukuId,
                   Tampilsiswa, EditSiswa,Hapussiswa,TampilSiswaId,
@@ -1129,5 +1144,5 @@ module.exports = { Tampilbuku, Tambahbuku, Editbuku, Hapusbuku,TampilbukuId,
                   Tampilkembali, Tambahkembali, Editkembali, Hapuskembali,tampilIdKembali,TampilKembaliId,TampilStokKembali,
                   Tampiladmin, TampilAdminId, Tambahadmin, Editadmin, Hapusadmin,
                   searchBook,searchSiswa,searchPinjam,searchPinjamUser,searchKembali,TampilSiswaUser,
-                  SetujuPinjam
+                  SetujuPinjam,Getme
                   };
